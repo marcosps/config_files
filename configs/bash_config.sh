8ab74645f8a094c7261f06e14f4036b3f035b1ad
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2018-03-07 00:15:12 -0300

    Export GOPATH/GOBIN, and remove kernel linux path
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

--- configs/bash_config.sh
+++ configs/bash_config.sh
@@ -1,10 +1,17 @@
 export EDITOR=vim
-export LINUX_GIT_PATH=/mnt/data/gitroot/linux:~/gitroot/linux
-# http://devel.fedoraproject.narkive.com/lE637KsY/malloc-perturb-everyone-should-set-this-envvar
-export export MALLOC_PERTURB_=$(($RANDOM % 255 + 1))
-eval $(thefuck --alias)
+eval "$(thefuck --alias)"
 # You can use whatever you want as an alias, like for Mondays:
-eval $(thefuck --alias FUCK)
+eval "$(thefuck --alias FUCK)"
+
+# set GO env vars
+GITROOT=~/data/gitroot
+if [ ! -d "$GITROOT" ]; then
+	GITROOT=~/gitroot
+fi
+
+export GOPATH=$GITROOT/go
+export GOBIN=$GITROOT/go/bin
+mkdir -p $GITROOT/{go,bin}
 
 # locales
 export LC_ALL=en_US.UTF-8
